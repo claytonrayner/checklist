142b2587d0c2e20729aafa696f5d2eb47cfeac87
claytonrayner <raynerclayton@gmail.com>
2022-08-10 21:35:28 -0600

    Updated checklist program to include count and previousTask support.

--- checklist/Program.cs
+++ checklist/Program.cs
@@ -2,15 +2,23 @@
 
 namespace Checklist // Note: actual namespace depends on the project name.
 {
+
     internal class Program
     {
-        private static void promptForTaskCompletion(string task)
+
+        private static void promptForTaskCompletion(TodoTask task)
         {
-            Console.WriteLine($"Did you finish your {task}? (y/n)");
+            var timeStr = task.count == 1 ? "time" : "times";
+            Console.WriteLine($"Did you finish your {task.name}? You were supposed to do it {task.count} {timeStr} (y/n)");
+
+            if (task.previousTask != null)
+            {
+                Console.WriteLine($"Remember, you were supposed to do this AFTER {task.previousTask.name}");
+            }
 
             while (Console.ReadLine() != "y")
             {
-                Console.WriteLine($"Ok. Did you finish your {task} yet? (y/n)");
+                Console.WriteLine($"Ok. Did you finish your {task.name} yet? (y/n)");
             }
         }
 
@@ -22,14 +30,45 @@ namespace Checklist // Note: actual namespace depends on the project name.
 
             Console.WriteLine("Time to get your tasks done...");
 
-            var tasks = new List<string> { "programming lesson", "writing", "speed painting", "run" };
+            var writingTask = new TodoTask
+            {
+                name = "writing",
+                count = 1
+            };
 
-            foreach(var t in tasks)
+            var tasks = new List<TodoTask> {
+                new TodoTask
+                {
+                    name = "programming lesson",
+                    count = 1
+                },
+                writingTask,
+                new TodoTask
+                {
+                    name = "speed painting",
+                    count = 1,
+                    previousTask = writingTask
+                },
+                new TodoTask
+                {
+                    name = "drink water",
+                    count = 8
+                },
+            };
+
+            foreach (var t in tasks)
             {
                 promptForTaskCompletion(t);
             }
 
             Console.WriteLine("Good for you homie. Clap clap clap.");
         }
+    }
+
+    internal class TodoTask
+    {
+        internal string name;
+        internal int count = 1;
+        internal TodoTask previousTask;
     }
 }
