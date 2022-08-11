6b3a7786e83d62f614c432a4670bf14d8aadaa4c
claytonrayner <raynerclayton@gmail.com>
2022-08-10 21:14:37 -0600

    Created app for tracking my tasks.

---
+++ checklist/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Checklist // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        private static void promptForTaskCompletion(string task)
+        {
+            Console.WriteLine($"Did you finish your {task}? (y/n)");
+
+            while (Console.ReadLine() != "y")
+            {
+                Console.WriteLine($"Ok. Did you finish your {task} yet? (y/n)");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var name = "Clayton";
+
+            Console.WriteLine("Hello master " + name);
+
+            Console.WriteLine("Time to get your tasks done...");
+
+            var tasks = new List<string> { "programming lesson", "writing", "speed painting", "run" };
+
+            foreach(var t in tasks)
+            {
+                promptForTaskCompletion(t);
+            }
+
+            Console.WriteLine("Good for you homie. Clap clap clap.");
+        }
+    }
+}
